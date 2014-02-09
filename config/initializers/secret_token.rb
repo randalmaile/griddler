8fc5b7b81243ba56ebb0f942ab3576fb67ca14d6
randalmaile <randalmaile@gmail.com>
2014-02-09 13:16:28 -0800

    Initial commit - blog test - email integration with Griddler/SendGrid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '03553a6ae4a0fbc0b490d9b9f02787ea041f36d6c493cfb85ef43e53445f86858aa463056c66d0956dd501859128a27cfd71e792574ab7b42c6945c1be1a2d34'
